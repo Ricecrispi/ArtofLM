a0aec8e61ec125567711081d407f61c0840aca4f
Christina <christina@artlocalapp.com>
2015-09-12 16:50:39 -0400

    file upload frontend implemented

--- client/app/artwork/artwork.controller.js
+++ client/app/artwork/artwork.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('artOfLmApp')
-  .controller('ArtworkCtrl', ['$scope', 'artwork', '$state', function ($scope, Artwork, $state) {
-    $scope.artwork = Artwork;
+  .controller('ArtworkCtrl', ['$scope', 'artwork', '$state', function ($scope, artwork, $state) {
+    $scope.artwork = artwork;
 
     $scope.create = function () {
-      artwork.create({name: $scope.name}, file).success(function (data) {
+      artwork.create({name: $scope.name}, $scope.file).success(function (data) {
         $state.go('main');
       });
 
