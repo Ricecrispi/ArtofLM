a4b5a940442a49d6fa71340e3a4df2be58c15905
Christina <christina@artlocalapp.com>
2015-09-14 00:07:13 -0400

    bug fix, couldnt add + delete art

--- client/app/artwork/artwork.controller.js
+++ client/app/artwork/artwork.controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('artOfLmApp')
-  .controller('ArtworkCtrl', ['$scope', 'artwork', '$state', function ($scope, artwork, $state) {
+  .controller('ArtworkCtrl', ['$scope', 'artwork', '$state', 'auth', function ($scope, artwork, $state, auth) {
+    $scope.art = {};
+
     $scope.create = function() {
-      artwork.create({name: $scope.name}, $scope.file).then(function (data) {
+      artwork.create({name: $scope.art.name}, $scope.art.file).then(function (data) {
         $state.go('main');
       });
     };
 
     $scope.isLoggedIn = function() {
       auth.isLoggedIn().then(function (data) {
-        $scope.loggedIn = data;
+        $scope.loggedIn = data.loggedIn;
       }, function () {
 
       });
